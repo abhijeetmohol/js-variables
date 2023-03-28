5e687f4096e47bfd500eca94bc4211a8e24f2d51
abhijeetmohol <abhijeetmohol3101@gmail.com>
2023-03-28 20:11:26 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+//  Jvascript variables
+console.log("Variables of WE ARE GROWING");
+var A="ABHIJEET"
+console.log(A);
+
+var B="OMKAR"
+console.log(B);
+
+var C="KUNAL"
+console.log(C);
+
+var D="PRASAD"
+console.log(D);
+
+var E="VINAYAK"
+console.log(E);
---
+++ assets/main.js
@@ -0,0 +1,20 @@
+//  Jvascript variables
+console.log("Variables of WE ARE GROWING");
+var A="ABHIJEET"
+console.log(A);
+
+var B="OMKAR"
+console.log(B);
+
+var C="KUNAL"
+console.log(C);
+
+var D="PRASAD"
+console.log(D);
+
+var E="VINAYAK"
+console.log(E);
+/*
+you can add
+more variables
+*/
